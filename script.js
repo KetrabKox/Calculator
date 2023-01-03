0a27d2726be884c75f5f9c9ea4da29e693552ebb
KetrabKox <coox144@gmail.com>
2023-01-03 13:55:10 +0100

    Added another functional buttons

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const buttons = document.querySelectorAll("button");
 let values=[];
 let connectValues
 
+function changeSign(){
+    if(values[0]==="-"){
+        values.shift();
+    }else{
+        values.unshift("-");
+    }
+    connectValues=values.join("");
+    screen.textContent=connectValues;
+}
+
 function calc(button) {
     const value = button.textContent;
     if (value === "C"){
@@ -11,12 +21,27 @@ function calc(button) {
         screen.textContent="0";
     } else if(value === "="){
         screen.textContent=eval(connectValues);
-    } else{
+    }else if(value === "+/-"){
+        changeSign();
+    }else if(value === "%"){
+        values.push("/100");
+        connectValues=values.join("");
+        screen.textContent=eval(connectValues);
+    }else if(value === "×"){
+        values.push("*");
+        connectValues=values.join("");
+        screen.textContent=connectValues;
+    }else if(value === "÷"){
+        values.push("/");
+        connectValues=values.join("");
+        screen.textContent=connectValues;
+    }else{
         values.push(value);
         connectValues=values.join("");
         screen.textContent=connectValues;
     }
 
 }
+
 
 buttons.forEach(button => {button.addEventListener("click", () => calc(button))});
